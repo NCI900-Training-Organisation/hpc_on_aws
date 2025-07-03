05b453488602b50224fbb6b43bb66092919d0331
Joseph John <joseph.john@anu.edu.au>
2025-07-03 11:52:48 +1000

    tag updated in output

--- sample_cluster/outputs.tf
+++ sample_cluster/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_ids" {
   description = "IDs of the EC2 instances"
-  value       = [for instance in aws_instance.app_server : instance.id]
+  value       = [for instance in aws_instance.cluster : instance.id]
 }
 
 output "instance_public_ips" {
   description = "Public IP addresses of the EC2 instances"
-  value       = [for instance in aws_instance.app_server : instance.public_ip]
+  value       = [for instance in aws_instance.cluster : instance.public_ip]
 }

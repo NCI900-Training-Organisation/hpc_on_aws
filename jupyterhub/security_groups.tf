dc5070efadcac81125a6eddff9a8387abc3c3868
Joseph John <joseph.john@anu.edu.au>
2026-05-05 19:59:54 +1000

    new playbooks for jupyterhub and python packages. Playbooks updated for ubuntu

--- jupyterhub/security_groups.tf
+++ jupyterhub/security_groups.tf
@@ -1,65 +1,67 @@
 # -----------------------------------------------------
-# Security Group: Allow SSH Access from Anywhere
+# Security Group: GPU Server Access (SSH + JupyterHub)
 # -----------------------------------------------------
 resource "aws_security_group" "ssh_access" {
-  # The name of the security group (visible in AWS Console)
-  name = "allow_ssh_from_anywhere"
 
-  # A short description for documentation
-  description = "Allow SSH inbound traffic"
+  name        = "allow_ssh_from_anywhere"
+  description = "Allow SSH and JupyterHub inbound traffic"
+  vpc_id      = aws_vpc.main.id
 
-  # The ID of the VPC where this security group will be created
-  vpc_id = aws_vpc.main.id
-
-  # -----------------------------
-  # Ingress Rule 1: IPv4 SSH Access
-  # -----------------------------
+  # -----------------------------------------------------
+  # SSH Access (Port 22)
+  # -----------------------------------------------------
   ingress {
-    # Rule description shown in AWS Console
     description = "SSH from anywhere (IPv4)"
-
-    # SSH uses TCP port 22, so allow traffic on port 22
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    # Allow incoming SSH connections from any IPv4 address
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # -----------------------------
-  # Ingress Rule 2: IPv6 SSH Access
-  # -----------------------------
-  ingress {
-    # Same as above, but for IPv6
-    description = "SSH from anywhere (IPv6)"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-
-    # Allow incoming SSH connections from any IPv6 address
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  # -----------------------------
-  # Egress Rule: Allow All Outbound Traffic
-  # -----------------------------
-  egress {
-    # Allow all types of outbound traffic (all protocols and ports)
-    from_port = 0
-    to_port   = 0
-
-    # "-1" means "all protocols"
-    protocol = "-1"
-
-    # Allow outgoing traffic to any IPv4 address
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # -----------------------------
-  # Tag for Easy Identification
-  # -----------------------------
+  ingress {
+    description      = "SSH from anywhere (IPv6)"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # -----------------------------------------------------
+  # JupyterHub HTTP Access (Port 80)
+  # -----------------------------------------------------
+  ingress {
+    description = "HTTP for JupyterHub (TLJH)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # -----------------------------------------------------
+  # Optional: HTTPS (if you later enable TLS)
+  # -----------------------------------------------------
+  ingress {
+    description = "HTTPS for JupyterHub"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # -----------------------------------------------------
+  # Allow All Outbound Traffic
+  # -----------------------------------------------------
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # -----------------------------------------------------
+  # Tags
+  # -----------------------------------------------------
   tags = {
-    Name = "GPU-Server-ssh-access" # This tag shows up in AWS Console
+    Name = "GPU-Server-ssh-access"
   }
 }
